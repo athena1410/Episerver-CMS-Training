2eaebbf45f4b98c10370e91028a594c3316d5723
duytoan <duytoan.mk@gmail.com>
2021-06-13 18:22:50 +0700

    Alloy Training: Install JQuery and Boostrap

---
+++ AlloyTraining/SiteGroupNames.cs
@@ -0,0 +1,9 @@
+﻿namespace AlloyTraining
+{
+    public static class SiteGroupNames
+    {
+        public const string SPECIALIZED = "Specialized";
+        public const string COMMON = "Common";
+        public const string NEWS = "News";
+    }
+}
